53bebc1725f73230520b9014f33255091b2f1e5c
Santino Cozzi <santino3101@gmail.com>
2026-08-12 16:10:40 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const string NombreKiosco = "KIOSCO EL RECREO";
+Console.WriteLine($"=== {NombreKiosco} ===");
+Console.Write("Nombre del cajero: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Bienvenido/a, {nombre}. Caja abierta.");
+
 Console.ReadLine();
